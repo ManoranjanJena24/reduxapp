cce0951db1cc963ef9f6e3e8dff0b6998973c512
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-07-28 18:00:10 +0530

    done with redux application

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const amount = useSelector(state => state.amount)
     return (
+
         <div>
+
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">State Bank Of Manoranjan</a>
@@ -21,7 +25,7 @@ const Navbar = () => {
 
                         </ul>
                         <div>
-                            <button disabled="true" className="btn btn-primary">Your Balance : 1000</button>
+                            <button disabled="true" className="btn btn-primary">Your Balance :{amount}</button>
                         </div>
                     </div>
                 </div>
--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from './state/index'
 
 const Shop = () => {
+    const dispatch = useDispatch();
     return (
         <div className='container text-center'>
             <h1>BUY ADIDDAS SHOES: 50RS</h1>
-            <button className="btn btn-primary">-</button>
+            <button className="btn btn-primary" onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }}>-</button>
             Add this Item to the cart
-            <button className="btn btn-primary">+</button>
+            <button className="btn btn-primary" onClick={() => { dispatch(actionCreators.depositMoney(100)) }}>+</button>
         </div>
     )
 }
